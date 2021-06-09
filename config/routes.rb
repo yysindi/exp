cfb2d41798c0ed57cb4b3989aa7ab67ba37ddd59
Cory Johnson <coryjohnson@protonmail.com>
2021-06-09 18:18:47 +0100

    added bell icon to navbar, still need to play around a bit with link_to's and applied.status?

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/:id', to: 'users#show'
+  get 'users/:id', to: 'users#show', as: :user
   root to: 'pages#home'
   # post 'apply', to: 'pages#apply'
   resources :jobs, only: [:index , :show] do 
