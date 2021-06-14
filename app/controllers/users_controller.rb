dede972a2cac94dff2d0164331c3b2c7ccccccf1
Ariella Morris <ariellax@gmail.com>
2021-06-14 12:43:37 +0100

    added complete jobs section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @applications = Application.where(user_id: @user.id)
-    @exp_scores = @user.exp_scores.group(:industry)
-
+    @exp_scores = @user.exp_scores
   end
 
   def edit
