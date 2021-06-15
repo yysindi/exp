f1d446c1ff0b048fd073a472f5bb91e80fcf8db7
Cory Johnson <coryjohnson@protonmail.com>
2021-06-15 17:19:16 +0100

    much better notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_counter
+
+  private
+
+  def set_counter
+    return if current_user.nil?
+    if current_user.notifications.all? { |note| note.read? }
+      @application_count = 0
+    else
+      @application_count = current_user.notifications.count { |note| !note.read? }
+    end
+  end
 end
