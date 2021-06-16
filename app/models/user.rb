35a073146842fd67cd81e7f3ca6bcceb01ebf8b5
steve fernandes <stevefernandes24@hotmail.co.uk>
2021-06-16 09:34:58 +0100

    deleted old version of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  after_create :send_welcome_email
   has_many :applications, dependent: :destroy
   has_many :exp_scores, dependent: :destroy
   has_many :jobs, through: :applications
@@ -14,9 +13,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  private
-
-  def send_welcome_email
-    UserMailer.with(user: self).welcome.deliver_now
-  end
 end
