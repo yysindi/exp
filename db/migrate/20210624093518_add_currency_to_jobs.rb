a358f79c2a158a32fa18e63c7845d9147f04f979
Cory Johnson <coryjohnson@protonmail.com>
2021-06-24 10:42:42 +0100

    removed currency field from jobs while I figure out money gem

--- db/migrate/20210624093518_add_currency_to_jobs.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrencyToJobs < ActiveRecord::Migration[6.0]
-  def change
-    add_column :jobs, :currency, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_093518) do
+ActiveRecord::Schema.define(version: 2021_06_23_165924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,6 @@ ActiveRecord::Schema.define(version: 2021_06_24_093518) do
     t.string "location"
     t.text "about"
     t.bigint "user_id"
-    t.float "currency"
     t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
